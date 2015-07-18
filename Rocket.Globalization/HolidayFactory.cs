ee2b8f508eb2bb662b39160728c45fdab3a904ae
John Tjust <john.tjust@gmail.com>
2015-07-18 19:08:58 +0200

    Links

--- Rocket.Globalization/HolidayFactory.cs
+++ Rocket.Globalization/HolidayFactory.cs
@@ -13,6 +13,15 @@ using Rocket.Globalization.Sweden;
 
 namespace Rocket.Globalization
 {
+    /// <summary>
+    /// http://www.samlogic.com/swedish-net-classes/classes/samlogic-calendar-library/class-swedish-calendar.htm
+    /// https://sv.wikipedia.org/wiki/Helgdagar_i_Sverige
+    /// http://rodadagar.com/
+    /// http://www.kalender.se/2012/November
+    /// http://www.dinstartsida.se/almanacka.asp?datum=2003-10-01
+    /// http://www.dinstartsida.se/almanacka.asp?datum=2015-02-01
+    /// http://www.dinstartsida.se/almanacka.asp?datum=2008-02-01
+    /// </summary>
     public class HolidayFactory : IHolidayFactory
     {
         public Holidays Create(Country country)
